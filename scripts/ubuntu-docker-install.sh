1d4996e41565066026ee1153a02efe994d6e5295
Reid Yonkers <81446209+chaiai@users.noreply.github.com>
2022-05-27 17:02:35 -0400

    Update ubuntu-docker-install.sh

--- scripts/ubuntu-docker-install.sh
+++ scripts/ubuntu-docker-install.sh
@@ -3,3 +3,15 @@
 # Ubuntu 22.04 LTS Server Docker installation and Kubenetes setup
 sudo apt update
 sudo apt -y full-upgrade
+
+# Prerequisites 
+sudo apt -y install curl nano net-tools ssh openssh-server
+
+# Docker / Kubernetes
+sudo curl -sSL https://get.docker.com/ | sh
+
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt update
